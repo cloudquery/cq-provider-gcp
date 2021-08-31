0b998d5422d76e3c4ebc170a0529b291d9dbad34
Andrii Romanenko <andrejjrom@gmail.com>
2021-08-31 17:36:23 +0300

    networks added

---
+++ resources/integration_tests/infra/network.tf
@@ -0,0 +1,29 @@
+resource "google_compute_network" "network" {
+  name = "network-${var.test_prefix}${var.test_suffix}"
+  mtu = 1500
+}
+
+resource "google_compute_network" "network1" {
+  name = "network1-${var.test_prefix}${var.test_suffix}"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "network-subnetwork" {
+  name = "sub-${var.test_prefix}${var.test_suffix}"
+  ip_cidr_range = "10.2.0.0/16"
+  network = google_compute_network.network.id
+  region = var.region
+  secondary_ip_range {
+    range_name = "range-${var.test_prefix}${var.test_suffix}"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+
+resource "google_compute_network_peering" "network-peering" {
+  name = "p-${var.test_prefix}${var.test_suffix}"
+  network = google_compute_network.network.id
+  peer_network = google_compute_network.network1.id
+}
+
+
+
--- resources/integration_tests/infra/provider.tf
+++ resources/integration_tests/infra/provider.tf
@@ -1,3 +1,4 @@
 provider "google" {
-  //  region  = "us-central1"
+    region  = var.region
 }
+
