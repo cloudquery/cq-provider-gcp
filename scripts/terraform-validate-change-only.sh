6f55733934bfb8a15de8f0b3499dea82b17620c5
Yevgeny Pats <yev.pats@gmail.com>
2022-03-20 14:47:59 +0200

    change change only script back to work without forks

--- scripts/terraform-validate-change-only.sh
+++ scripts/terraform-validate-change-only.sh
@@ -5,7 +5,7 @@ set -e
 for f in terraform/*; do
     if [ -d $f ]; then
         cd $f/prod
-        CHANGES=$(git diff --name-only origin/main ..)
+        CHANGES=$(git diff --name-only HEAD..origin/main ..)
         # if there are any changes run terraform apply
         if [ "$CHANGES" != "" ]; then
             echo "detected changes in $f. Running terraform apply..."
