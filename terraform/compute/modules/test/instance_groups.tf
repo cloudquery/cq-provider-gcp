7240c900ef66e394a7b3379320679527dc8a2264
Andrii Romanenko <andrejjrom@gmail.com>
2022-05-03 11:41:37 +0300

    fix

--- terraform/compute/modules/test/instance_groups.tf
+++ terraform/compute/modules/test/instance_groups.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance_group" "test" {
-  name        = "${var.region}-instance-group"
+  name        = "${var.prefix}-instance-group"
   description = "Integration test instance group"
   zone        = "${var.region}-a"
   network     = module.vpc.network_id
