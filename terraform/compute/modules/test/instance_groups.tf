4bbdf29aa7f882792a2ab36c9847f5f3dd09994b
Andrii Romanenko <andrejjrom@gmail.com>
2022-05-03 09:42:25 +0300

    removed tf changes

--- terraform/compute/modules/test/instance_groups.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_instance_group" "test" {
-  name        = "${var.region}-instance-group"
-  description = "Integration test instance group"
-  zone        = "us-central1-a"
-  network     = module.vpc.network_id
-}
