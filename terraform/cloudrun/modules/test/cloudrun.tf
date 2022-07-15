8c04ef71f10b4ed650cbba094a1f312cdd9b4f6b
Herman Schaaf <hermanschaaf@gmail.com>
2022-07-15 09:39:17 +0100

    Set metadata

--- terraform/cloudrun/modules/test/cloudrun.tf
+++ terraform/cloudrun/modules/test/cloudrun.tf
@@ -10,12 +10,32 @@ resource "google_cloud_run_service" "default" {
     spec {
       containers {
         image = "us-docker.pkg.dev/cloudrun/container/hello"
+        env {
+          name = "ENV_VAR"
+          value = "test"
+        }
       }
+    }
+    metadata {
+      name = "${lower(var.prefix)}-cloudrun-srv-green"
+    }
+  }
+
+  metadata {
+    annotations = {
+      generated-by = "magic-modules"
     }
   }
 
   traffic {
     percent         = 100
     latest_revision = true
+  }
+  autogenerate_revision_name = false
+
+  lifecycle {
+    ignore_changes = [
+      metadata.0.annotations,
+    ]
   }
 }
