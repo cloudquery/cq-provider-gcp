5e455db8591ac3048cf76e964f57e3387b561d52
Kemal Hadimli <disq@users.noreply.github.com>
2022-07-15 10:43:42 +0100

    Fix include paths

--- policies/sql/cis_v1.2.0/section_7.sql
+++ policies/sql/cis_v1.2.0/section_7.sql
@@ -2,10 +2,10 @@
 \echo "Executing CIS V1.2.0 Section 7"
 \set check_id "7.1"
 \echo "Executiong check 7.1"
-\i gcp/queries/bigquery/datasets_publicly_accessible.sql
+\i sql/queries/bigquery/datasets_publicly_accessible.sql
 \set check_id "7.2"
 \echo "Executiong check 7.2"
-\i gcp/queries/bigquery/tables_not_encrypted_with_cmek.sql
+\i sql/queries/bigquery/tables_not_encrypted_with_cmek.sql
 \set check_id "7.3"
 \echo "Executiong check 7.3"
-\i gcp/queries/bigquery/datasets_without_default_cmek.sql
+\i sql/queries/bigquery/datasets_without_default_cmek.sql
