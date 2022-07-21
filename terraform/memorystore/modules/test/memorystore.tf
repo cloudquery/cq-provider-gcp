d0e77a60cee7bf2d4e501d5a88411db29992ec7d
candiduslynx <candiduslynx@gmail.com>
2022-07-21 16:00:44 +0300

    added API as well

--- terraform/memorystore/modules/test/memorystore.tf
+++ terraform/memorystore/modules/test/memorystore.tf
@@ -1,6 +1,13 @@
+resource "google_project_service" "memorystore" {
+  project            = var.project_id
+  service            = "redis.googleapis.com"
+  disable_on_destroy = false
+}
+
 module "memorystore" {
-  source  = "terraform-google-modules/memorystore/google"
-  version = "~> 4.4.0"
+  depends_on = [google_project_service.memorystore]
+  source     = "terraform-google-modules/memorystore/google"
+  version    = "~> 4.4.0"
 
   name    = "${var.prefix}-memorystore"
   project = var.project_id
